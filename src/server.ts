80607000f6125f6e0965c1e4a1ca899c83731f5c
mdxshakil <mdxshakil1767@gmail.com>
2023-06-01 20:05:46 +0600

    [DONE] Crearted user interface and model

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function dbConnect() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log("Db Connected 💾");
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Db Connected 💾')
 
-        app.listen(config.port, () => {
-            console.log(`Server is running on port ${config.port} 🚀`)
-        })
-    } catch (error) {
-        console.log("Failed to connect", error);
-    }
+    app.listen(config.port, () => {
+      console.log(`Server is running on port ${config.port} 🚀`)
+    })
+  } catch (error) {
+    console.log('Failed to connect', error)
+  }
 }
 
-dbConnect();
+dbConnect()
