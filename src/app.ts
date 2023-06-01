9c3855f25f1ba1961b1a42bdfa3e69e21a4a29ab
mdxshakil <mdxshakil1767@gmail.com>
2023-06-01 20:01:38 +0600

    [DONE] Setup eslint, prettier, husky, lint staged

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,14 @@
-import express, { Request,Response } from "express";
-import cors from "cors";
-const app = express();
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+const app = express()
 
 // middlewares
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
-
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
-export default app;
+export default app
