11c0a83f4497c89168e83f2203d24efb6891c0ed
mdxshakil <mdxshakil1767@gmail.com>
2023-06-06 11:57:48 +0600

    [Done]  Create Academic Semester Interface , Model

--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,45 @@
-import { Server } from 'http'
-import mongoose from 'mongoose'
-import app from './app'
-import config from './config'
-import { errorLogger, logger } from './shared/logger'
+import { Server } from 'http';
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
+import { errorLogger, logger } from './shared/logger';
 
 process.on('uncaughtException', error => {
-  errorLogger.error(error)
-  process.exit(1)
-})
+  errorLogger.error(error);
+  process.exit(1);
+});
 
-let server: Server
+let server: Server;
 async function dbConnect() {
   try {
-    await mongoose.connect(config.database_url as string)
-    logger.info('Db Connected 💾')
+    await mongoose.connect(config.database_url as string);
+    logger.info('Db Connected 💾');
 
     server = app.listen(config.port, () => {
-      logger.info(`Server is running on port ${config.port} 🚀`)
-    })
+      logger.info(`Server is running on port ${config.port} 🚀`);
+    });
   } catch (error) {
-    errorLogger.error('Failed to connect', error)
+    errorLogger.error('Failed to connect', error);
   }
 
   //terminate server gracefully after any unhandled rejection occurs
   process.on('unhandledRejection', error => {
     if (server) {
       server.close(() => {
-        errorLogger.error(error)
-        process.exit(1)
-      })
+        errorLogger.error(error);
+        process.exit(1);
+      });
     } else {
-      process.exit(1)
+      process.exit(1);
     }
-  })
+  });
 }
 
-dbConnect()
+dbConnect();
 
 process.on('SIGTERM', () => {
-  logger.info('SIGTERM is received')
+  logger.info('SIGTERM is received');
   if (server) {
-    server.close()
+    server.close();
   }
-})
+});
