504834585e29aab7be827e7bb39676f5a5598b26
mdxshakil <mdxshakil1767@gmail.com>
2023-06-08 13:47:05 +0600

    Fixed zod validation error for id

--- src/app/modules/academicSemester/academicSemster.controller.ts
+++ src/app/modules/academicSemester/academicSemster.controller.ts
@@ -1,11 +1,11 @@
-import { RequestHandler } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
 import { AcademicSemesterService } from './academicSemester.services';
 
-const createAcademicSemester: RequestHandler = catchAsync(
-  async (req, res, next) => {
+const createAcademicSemester = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const { ...acdemicSemseterData } = req.body;
     const result = await AcademicSemesterService.createSemester(
       acdemicSemseterData
