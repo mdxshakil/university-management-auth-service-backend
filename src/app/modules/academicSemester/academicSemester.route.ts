d2cf64da028f3f65d0bf858c48413155358e1f0f
mdxshakil <mdxshakil1767@gmail.com>
2023-06-10 12:18:52 +0600

    [Done] Update single semester

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -12,5 +12,6 @@ router.post(
 
 router.get('/:id', AcademicSemesterController.getSingleSemester);
 router.get('/', AcademicSemesterController.getAllSemesters);
+router.patch('/:id', AcademicSemesterController.updateSemester);
 
 export const AcademicSemesterRoute = router;
