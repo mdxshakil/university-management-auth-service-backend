0da1ddcce624e0f4a37d44e7bd9ef07fc69e0f52
mdxshakil <mdxshakil1767@gmail.com>
2023-06-11 12:47:55 +0600

    [Done] Implemented AcademicFaculty Module

---
+++ src/app/modules/academicFaculty/academicFaculty.validate.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+
+const createAcademicFacultyZodSchema = z.object({
+  body: z.object({
+    title: z.string({
+      required_error: 'Faculty title is required',
+    }),
+  }),
+});
+
+const updatefacultyZodSchema = z.object({
+  body: z.object({
+    title: z.string({
+      required_error: 'Title is required',
+    }),
+  }),
+});
+
+export const AcademicFacultyValidation = {
+  createAcademicFacultyZodSchema,
+  updatefacultyZodSchema,
+};
